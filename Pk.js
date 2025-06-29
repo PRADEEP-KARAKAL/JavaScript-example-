68e048cee61c8b88f44b93d7f46b0242d3ef74ec
PRADEEP-KARAKAL <pradeepkarakal67@gmail.com>
2025-06-29 16:37:07 +0530

    Prk.js

--- Pk.js
+++ Pk.js
@@ -1,1 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Simple JavaScript Example</title>
+</head>
+<body>
+
+<h2>Click the Button</h2>
+
+<button onclick="showMessage()">Click Me!</button>
+
+<p id="message"></p>
+
+<script>
+  function showMessage() {
+    let msg = "Hello! You clicked the button.";
+    document.getElementById("message").innerText = msg;
+  }
+</script>
+
+</body>
+</html>
 
